f44ca66c8797399799b4810802d91574502b2d46
jakubkala <kalajakub96@gmail.com>
2020-07-14 21:45:44 +0200

    create_simulation_data rebuilt - pipes not longer used

--- R/create_simulation_data.R
+++ R/create_simulation_data.R
@@ -3,18 +3,18 @@
 #' @param replications number of matrices' generation for each set of parameters
 #' @param seq_nums vector of sequences' numbers
 #' @param seq_lengths vector of sequences' lengths
-#' @param n_motifs vector of number of motifs to be injected
+#' @param num_motifs vector of number of motifs to be injected
 #' @param alphabet elements used to build both sequence and motif
 #' @param path file path of RDS files
 #' @param title simulation name
 #' @param save_files triggers if RDS will be saved
 #' @return data frame with RDS files' details
-#' @importFrom pbapply pblapply
 #' @importFrom utils write.csv
+#' @importFrom progress progress_bar
 #' @export
 #' @examples
 #' alph <- 1L:4
-#' reps <- 1
+#' reps <- 10
 #' n_seq <- c(25)
 #' l_seq <- c(8)
 #' n_motifs <- c(1, 2)
@@ -24,7 +24,7 @@
 create_simulation_data <- function(replications,
                                    seq_nums,
                                    seq_lengths,
-                                   n_motifs,
+                                   num_motifs,
                                    alphabet,
                                    path,
                                    title,
@@ -33,15 +33,26 @@ create_simulation_data <- function(replications,
   if (!file.exists(path))
     stop("Output directory does not exist.")
 
-  sim_quipt <- pblapply(1L:replications, function(replication) {
-    lapply(seq_nums, function(n_seq) {
-      lapply(seq_lengths, function(l_seq) {
-        lapply(n_motifs, function(n_motifs){
+  df <- data.frame(replication = integer(),
+                   n_seq = integer(),
+                   l_seq = integer(),
+                   n_motifs = integer(),
+                   path = character())
 
+  totalIterations = replications * length(seq_nums) * length(seq_lengths) *  length(num_motifs)
+  pb <- progress_bar$new(format = "[:bar] :current/:total (:percent)",
+                         total = totalIterations)
+  pb$tick(0)
+
+  for (replication in 1:replications) {
+    for (n_seq in seq_nums) {
+      for (l_seq in seq_lengths) {
+        for (n_motifs in num_motifs) {
+
+          pb$tick(1)
           set.seed(replication)
           motifs <- generate_motifs(alphabet, n_motifs)
           dat <- generate_sequences(n_seq, l_seq, alphabet, motifs, n_motifs)
-
 
           filePath = paste0(path, title,
                             "_rep_", replication,
@@ -53,26 +64,22 @@ create_simulation_data <- function(replications,
           if (save_files)
             saveRDS(dat, file = filePath)
 
-          data.frame(replication = replication,
+          newdf <- data.frame(replication = replication,
                      n_seq = n_seq,
                      l_seq = l_seq,
                      n_motifs = n_motifs,
-                     path = filePath
-          )
-        }) %>%
-          do.call(rbind, .)
-      }) %>%
-        do.call(rbind, .)
-    }) %>%
-      do.call(rbind, .)
-  }) %>%
-    do.call(rbind, .)
+                     path = filePath)
+
+          df <- rbind(df, newdf)
+
+        }
+      }
+    }
+  }
 
   if (save_files)
-    write.csv(sim_quipt, file = paste0(path, title,".csv"))
+    write.csv(df, file = paste0(path, title,".csv"))
 
-  sim_quipt
-
+  df
 }
-
 
