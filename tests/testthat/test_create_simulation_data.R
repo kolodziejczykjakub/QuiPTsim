e2226fce4b6ae12f9c4429e248f29dfe7a1c97f2
jakubkala <kalajakub96@gmail.com>
2020-07-16 23:48:56 +0200

    create_simulation_data tests

--- tests/testthat/test_create_simulation_data.R
+++ tests/testthat/test_create_simulation_data.R
@@ -8,12 +8,13 @@ test_that("Correct result of create_simulation_data", {
   n_seq <- c(10)
   l_seq <- c(10)
   n_motifs <- 1
+  path <- "./"
   results <- create_simulation_data(reps, n_seq, l_seq, n_motifs, alph,
                                     path, "SEQ", FALSE,
                                     motifProbs = c(0.7, 0.1, 0.1, 0.1),
                                     seqProbs = c(0.7, 0.1, 0.1, 0.1),
                                     n = 4, d = 4)
-  results_true <- readRDS("./tests/testthat/test_create_simulation_data_df.Rds")
+  results_true <- readRDS("./test_create_simulation_data_df.Rds")
 
   expect_equal(results_true, results)
 })
