7faae88de28966d8628e7a3430d383343bad7451
jakubkala <kalajakub96@gmail.com>
2020-07-16 23:36:37 +0200

    test of create_simulation_data

---
+++ tests/testthat/test_create_simulation_data.R
@@ -0,0 +1,19 @@
+context("QuiPT simulation")
+
+test_that("Correct result of create_simulation_data", {
+
+  set.seed(1)
+  alph <- 1L:4
+  reps <- 3
+  n_seq <- c(10)
+  l_seq <- c(10)
+  n_motifs <- 1
+  results <- create_simulation_data(reps, n_seq, l_seq, n_motifs, alph,
+                                    path, "SEQ", FALSE,
+                                    motifProbs = c(0.7, 0.1, 0.1, 0.1),
+                                    seqProbs = c(0.7, 0.1, 0.1, 0.1),
+                                    n = 4, d = 4)
+  results_true <- readRDS("./tests/testthat/test_create_simulation_data_df.Rds")
+
+  expect_equal(results_true, results)
+})
