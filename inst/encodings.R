7d190f65d41086afb6cd03c07f318940ac3b77c3
jakubkala <kalajakub96@gmail.com>
2020-07-29 18:56:12 +0200

    encoding probs

--- inst/encodings.R
+++ inst/encodings.R
@@ -11,6 +11,61 @@ for (col in c("AUC_mean", "MCC_mean", "Sens_mean", "Spec_mean", "AUC_sd",
   vis_dat_fit_best[[col]] <- as.numeric(vis_dat_fit_best[[col]])
 }
 
+#
+PATH = "~/amylogram/AmyloGramAnalysis/"
+
+library(seqinr)
+source(paste0(PATH, "./functions/encode_amyloids.R"))
+
+load(paste0(PATH, "./data/aa_groups.RData"))
+aa_groups <- string2list(aa_groups)
+
+raw_seqs_list <- c(read.fasta(paste0(PATH, "./data/amyloid_pos_full.fasta"),seqtype = "AA"),
+                   read.fasta(paste0(PATH, "./data/amyloid_neg_full.fasta"),seqtype = "AA"))
+
+#sequences longer than 5 aa and shorter than 26 aa
+purified_seqs_id <- lengths(raw_seqs_list) > 5 & lengths(raw_seqs_list) < 26
+seqs_list <- raw_seqs_list[purified_seqs_id]
+
+seqs_m <- tolower(t(sapply(seqs_list, function(i)
+  c(i, rep(NA, max(lengths(seqs_list)) - length(i))))))
+
+raw_seqs_positive <- read.fasta(paste0(PATH, "./data/amyloid_pos_full.fasta"),seqtype = "AA")
+raw_seqs_negative <- read.fasta(paste0(PATH, "./data/amyloid_neg_full.fasta"),seqtype = "AA")
+
+seqs_list_positive <- raw_seqs_positive[lengths(raw_seqs_positive) > 5 & lengths(raw_seqs_positive) < 26]
+seqs_list_negative <- raw_seqs_negative[lengths(raw_seqs_negative) > 5 & lengths(raw_seqs_negative) < 26]
+
+seqs_m_pos <- tolower(t(sapply(seqs_list_positive, function(i)
+  c(i, rep(NA, max(lengths(seqs_list_positive)) - length(i))))))
+
+seqs_m_neg <- tolower(t(sapply(seqs_list_negative, function(i)
+  c(i, rep(NA, max(lengths(seqs_list_negative)) - length(i))))))
+
+occ_positive <- table(seqs_m_pos)
+occ_negative <- table(seqs_m_neg)
+
+# delete "-" from occurences
+occ_positive <- occ_positive[2:length(occ_positive)]
+
+probs_positive <- occ_positive / sum(occ_positive)
+probs_negative <- occ_negative / sum(occ_negative)
+
+# biogram::degenerate
+create_encoded_probabilites <- function(encoding, probs_positive, probs_negative) {
+
+  new_names_pos <- biogram::degenerate(names(probs_positive), encoding)
+  new_names_neg <- biogram::degenerate(names(probs_negative), encoding)
+
+  new_motifProbs <- unlist(lapply(1:length(encoding),
+                                  function(x) sum(probs_positive[new_names_pos == x])))
+  new_seqProbs <- unlist(lapply(1:length(encoding),
+                                function(x) sum(probs_negative[new_names_neg == x])))
+
+  list(positive = new_motifProbs,
+       negative = new_seqProbs)
+
+}
 
 #add as a benchmark two encodings from the literature
 aa1 = list(`1` = c("g", "a", "p", "v", "l", "i", "m"),
@@ -33,43 +88,19 @@ amylogram_model_encoding <- list(`1` = "g",
                                  `5` = c("a", "c", "h", "m"),
                                  `6` = c("d","e", "n", "q", "s", "t"))
 
-#
-PATH = "~/amylogram/AmyloGramAnalysis/"
 
-library(seqinr)
-source(paste0(PATH, "./functions/encode_amyloids.R"))
-
-load(paste0(PATH, "./data/aa_groups.RData"))
-aa_groups <- string2list(aa_groups)
-
-raw_seqs_list <- c(read.fasta(paste0(PATH, "./data/amyloid_pos_full.fasta"),seqtype = "AA"),
-                   read.fasta(paste0(PATH, "./data/amyloid_neg_full.fasta"),seqtype = "AA"))
-#sequences longer than 5 aa and shorter than 26 aa
-purified_seqs_id <- lengths(raw_seqs_list) > 5 & lengths(raw_seqs_list) < 26
-seqs_list <- raw_seqs_list[purified_seqs_id]
-
-seqs_m <- tolower(t(sapply(seqs_list, function(i)
-  c(i, rep(NA, max(lengths(seqs_list)) - length(i))))))
+encodingProbs <- list(amylogram_encoding = create_encoded_probabilites(amylogram_model_encoding,
+                                                                       probs_positive,
+                                                                       probs_negative),
+                      aa1 = create_encoded_probabilites(aa1,
+                                                        probs_positive,
+                                                        probs_negative),
+                      aa2 = create_encoded_probabilites(aa2,
+                                                        probs_positive,
+                                                        probs_negative))
 
 
-# biogram::degenerate
-# example
+library(QuiPTsim)
+lapply(encodingProbs, function(x) cosine_similarity(x$positive, x$negative))
 
-
-# weights
-weights$positive$`[11,19]`
-
-motifProbs <- weights$positive$`[11,19]`
-seqProbs <- weights$negative$`[11,19]`
-
-new_names_motif <-biogram::degenerate(tolower(names(weights$positive$`[11,19]`)), amylogram_model_encoding)
-new_names_seq <-biogram::degenerate(tolower(names(weights$negative$`[11,19]`)), amylogram_model_encoding)
-
-new_motifProbs <- unlist(lapply(1:6, function(x) sum(motifProbs[new_names_motif == x])))
-new_seqProbs <- unlist(lapply(1:6, function(x) sum(seqProbs[new_names_seq == x])))
-
-cosine_similarity(new_motifProbs, new_seqProbs)
-
-
-#
-table(c(seqs_m)) / sum(table(c(seqs_m)))
+saveRDS(encodingProbs, "./inst/encodingProbs.Rds")
