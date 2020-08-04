e703d0385b7a468d6538d3c744bfa3469372ef14
jakubkala <kalajakub96@gmail.com>
2020-08-04 12:48:57 +0200

    read ngram matrix

--- R/QuiPTevaluation.R
+++ R/QuiPTevaluation.R
@@ -22,6 +22,13 @@ subset_matrix <- function(filePath, n, fraction) {
   sampled_matrix
 }
 
+# wrapper for subset_matrix
+read_ngram_matrix <- function(filePath, n = NULL, fraction = 0.5) {
+  if (is.null(n)) matrix <- readRDS(filePath)
+  else matrix <- subset_matrix(filePath, n, fraction)
+  matrix
+}
+
 #' test QuiPT
 #' @param simulated_seqs sequences that will be tested
 #' @param n_seq number of sequences in each category
@@ -40,22 +47,55 @@ subset_matrix <- function(filePath, n, fraction) {
 #' simulated_sequences <- generate_sequences(n_seq, len, alph, motifs, 1)
 #' test_quipt(simulated_sequences, n_seq, criterion = "ig", motifs = motifs)
 
-# test_quipt <- function(simulated_seqs, n_seq, criterion, motifs, adjustment = "BH") {
-#
-#   res_df <- data.frame(biogram::test_features(target = c(rep(1, n_seq),
-#                                               rep(0, n_seq)),
-#                                    features = simulated_seqs))
-#
-#
-#   data.frame(#n_seq = n_seq,
-#     #l_seq = l_seq,
-#     res_df,
-#     motif = res_df[["ngram"]] %in% biogram::code_ngrams(sapply(motifs,
-#                                                       paste0,
-#                                                       collapse = "")),
-#     p.value.adj = p.adjust(res_df[["p.value"]], adjustment))
-# }
+test_quipt <- function(simulated_seqs, n_seq, criterion, motifs, adjustment = "BH") {
 
+  res_df <- data.frame(biogram::test_features(target = c(rep(1, n_seq),
+                                              rep(0, n_seq)),
+                                   features = simulated_seqs))
+
+
+  data.frame(#n_seq = n_seq,
+    #l_seq = l_seq,
+    res_df,
+    motif = res_df[["ngram"]] %in% biogram::code_ngrams(sapply(motifs,
+                                                      paste0,
+                                                      collapse = "")),
+    p.value.adj = p.adjust(res_df[["p.value"]], adjustment))
+}
+
+QuiPT_summary <- function(ngram_matrix) {
+
+  # create list of unique motifs
+  unique_motifs <- unique(unlist(attr(dat, "motifs"), recursive = FALSE))
+
+  # iterate over motifs
+  lapply(unique_motifs, function(motif) {
+
+    res_df <- data.frame(biogram::test_features(target = attr(ngram_matrix, "target"),
+                                                features = ngram_matrix))
+
+    # TODO: all adjustments at once?
+    adjustment <- "BH"
+
+    data.frame(
+      res_df,
+      motif = res_df[["ngram"]] %in% biogram::code_ngrams(sapply(motifs,
+                                                                 paste0,
+                                                                 collapse = "")),
+      p.value.adj = p.adjust(res_df[["p.value"]], adjustment)) -> res
+
+    # ngram contains motif
+    noi <- grepl(decode_ngrams(single_motif), decode_ngrams(res[["ngram"]]))
+    res[["contains.motif"]] <- noi
+
+    # ngram is a part of a motif
+    sapply(res[["ngram"]], function(single_ngram)
+      grepl(decode_ngrams(single_ngram), decode_ngrams(single_motif))) -> noi2
+    res[["motif.part"]] <- noi2
+
+    res
+  })
+}
 
 
 
