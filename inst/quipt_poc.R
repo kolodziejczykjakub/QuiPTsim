6e786f4c380f5cc1753f5c9ab39f495eb0c85a85
jakubkala <kalajakub96@gmail.com>
2020-08-06 15:20:44 +0200

    metrics

--- inst/quipt_poc.R
+++ inst/quipt_poc.R
@@ -1,14 +1,57 @@
 results <- read.csv("./inst/example_quipt_results.csv")
 
-# True positive
-# both should be considered as positive?
-true_positives <- results[(results$contains.motif | results$motif.part) & (results$p.value <= 0.05), ]
 
-# True negative
-true_negatives <- results[!(results$contains.motif | results$motif.part) & !(results$p.value <= 0.05), ]
+m1 <- read_ngram_matrix("./reduced_alph_enc_alph4_const/alph4_const_rep_1_seqNum_600_seqLen_10_nMotifs_3.Rds")
+m2 <- read_ngram_matrix("./reduced_alph_enc_alph4_const/alph4_const_rep_2_seqNum_600_seqLen_10_nMotifs_3.Rds")
 
-# False positive
-false_positives <- results[!(results$contains.motif | results$motif.part) & (results$p.value <= 0.05), ]
+m3 <- rbind_ngram_matrices(m1, m2)
 
-# False negative
-false_negatives <- results[(results$contains.motif | results$motif.part) & !(results$p.value <= 0.05), ]
+metrics <- function(results) {
+
+  # True positive
+  # both should be considered as positive?
+  true_positives <- sum((results$contains.motif | results$motif.part) & (results$p.value <= 0.05))
+
+  # True negative
+  true_negatives <- sum(!(results$contains.motif | results$motif.part) & !(results$p.value <= 0.05))
+
+  # False positive
+  false_positives <- sum(!(results$contains.motif | results$motif.part) & (results$p.value <= 0.05))
+
+  # False negative
+  false_negatives <- sum((results$contains.motif | results$motif.part) & !(results$p.value <= 0.05))
+
+  cat(paste0("TPR(sens):\n", true_positives / (true_positives + false_negatives)), "\n")
+  cat(paste0("TNR(spec):\n", true_negatives / (true_negatives + false_positives)), "\n")
+}
+
+metrics2 <- function(results) {
+
+  # True positive
+  # both should be considered as positive?
+  true_positives <- sum((results$motif.part) & (results$p.value <= 0.05))
+
+  # True negative
+  true_negatives <- sum(!(results$motif.part) & !(results$p.value <= 0.05))
+
+  # False positive
+  false_positives <- sum(!(results$motif.part) & (results$p.value <= 0.05))
+
+  # False negative
+  false_negatives <- sum((results$motif.part) & !(results$p.value <= 0.05))
+
+  cat(paste0("TPR(sens):\n", true_positives / (true_positives + false_negatives)), "\n")
+  cat(paste0("TNR(spec):\n", true_negatives / (true_negatives + false_positives)), "\n")
+}
+
+results1 <- QuiPT_summary(m1)
+results2 <- QuiPT_summary(m2)
+results3 <- QuiPT_summary(m3)
+metrics(results1)
+metrics(results2)
+metrics(results3)
+
+
+metrics2(results1)
+metrics2(results2)
+metrics2(results3)
