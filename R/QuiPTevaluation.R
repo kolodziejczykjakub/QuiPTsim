8cf334de893d9ed2dc5515e489d396ca7bd6b615
jakubkala <kalajakub96@gmail.com>
2020-08-10 08:06:00 +0200

    markovchain added to QuiPT summary

--- R/QuiPTevaluation.R
+++ R/QuiPTevaluation.R
@@ -56,6 +56,7 @@ read_ngram_matrix <- function(filePath, n = NULL, fraction = 0.5) {
 
 #' test QuiPT
 #' @param ngram_matrix matrix of n-gram occurences (computed in [create_simulation_data()])
+#' @param mc markovchain object to calculate n-gram probability
 #' @param adjustments p-value adjustments for multiple testing
 #' @param thresholds p-value thresholds
 #' @return data frame with results of QuiPT testing
@@ -65,6 +66,7 @@ read_ngram_matrix <- function(filePath, n = NULL, fraction = 0.5) {
 #' @export
 
 QuiPT_summary <- function(ngram_matrix,
+                          mc = NULL,
                           adjustments = c("holm", "BH"),
                           thresholds = c(0.05, 0.01)) {
 
@@ -77,23 +79,33 @@ QuiPT_summary <- function(ngram_matrix,
   res[adjustments] <- lapply(adjustments, function(p.adj) p.adjust(res[["p.value"]], p.adj))
   res[paste0("pval", thresholds)] <- lapply(thresholds, function(th) res[["p.value"]] < th)
 
+  if (!is.null(mc)) {
+    res[["prob"]] <- pblapply(strsplit(decode_ngrams(res[["ngram"]]), ""),
+                              function(ngram) calculate_ngram_prob(mc, ngram))
+  }
+
   # iterate over motifs to create vector of occurences
   motifOcc <- lapply(unique_motifs, function(motif) {
 
     motifOcc <- list()
 
     # ngram contains motif
-    noi <- grepl(gsub("_", ".", paste0(motif, collapse = "")), decode_ngrams(res[["ngram"]]))
+    noi <- grepl(paste0(motif, collapse = ""), decode_ngrams(res[["ngram"]]))
     motifOcc[["contains.motif"]] <- noi
 
     # ngram is a part of a motif
     noi2 <- sapply(res[["ngram"]], function(single_ngram)
-      grepl(gsub("_", ".", paste0(decode_ngrams(single_ngram), collapse = "")), paste0(motif, collapse = "")))
+      grepl(paste0(decode_ngrams(single_ngram), collapse = ""), paste0(motif, collapse = "")))
     motifOcc[["motif.part"]] <- noi2
 
     # exact motif
     noi3 <- (res[["ngram"]] == code_ngrams(paste0(motif, collapse = "")))
     motifOcc[["motif"]] <- noi3
+
+    positiveMotifs <- positive_motifs(paste0(motif, collapse = ""))
+    noi4 <- sapply(res[["ngram"]], function(single_ngram)
+      paste0(decode_ngrams(single_ngram), collapse = "") %in% positiveMotifs)
+    motifOcc[["positive"]] <- noi4
 
     motifOcc
   })
@@ -139,3 +151,10 @@ rbind_ngram_matrices <- function(m1, m2) {
   m_extended
 }
 
+positive_motifs <- function(motif) {
+  possibleWildcards <- (length(motif)+1) %/% 4
+  g <- expand.grid(0:possibleWildcards, 0:possibleWildcards)
+  WildcardsGrid <- g[apply(g, 1, sum) <= possibleWildcards, ]
+  possible_motifs <- apply(WildcardsGrid, 1, function(x) c(rep(".", x[1]), motif, rep(".", x[2])))
+  lapply(possible_motifs, function(x) paste0(x, collapse = ""))
+}
