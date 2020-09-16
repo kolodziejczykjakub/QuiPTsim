4a5fdbb0f4a6e41f048dd8da4b16b2dd3f26c866
jakubkala <kalajakub96@gmail.com>
2020-09-16 10:14:52 +0000

    add_unigrams

--- inst/add_unigrams.R
+++ inst/add_unigrams.R
@@ -1,24 +1,32 @@
 # > readRDS("../data/alph20_prob_(19,26]/alph20_prob_(19,26]_rep_1_seqNum_6000_seqLen_80_nMotifs_1.Rds")
 # A 6000x12209151 simple triplet matrix.
 library(pbapply)
+library(QuiPTsim)
 
-add_unigrams <- function(paths, alphabet) {
+add_unigrams <- function(old_dir, new_dir, RDSnames, alphabet) {
   
-  pblapply(paths, function(path) {
-    m <- readRDS(path)
+  pblapply(RDSnames, function(n) {
+    m <- readRDS(paste0(old_dir, n))
     m_with_uni <- cbind(count_ngrams(attr(m, "sequences"), alphabet = alphabet, 1), m)
     
     for (a in c("sequences", "motifs", "masks", "target")) {
       attr(m_with_uni, a) <- attr(m, a)
     }
-    
-    saveRDS(object = m_with_uni, file = path)
+    new_path <- paste0(new_dir, n)
+    saveRDS(object = m_with_uni, file = new_path)
   })
 }
 
-path <- "../data/reduced_alph_enc_amylogram_encoding/"
-paths <- dir("../data/reduced_alph_enc_amylogram_encoding/")
-df <- read.csv(paste0(path, paths[grepl("csv", paths)]))
-ngram_matrices_paths <-  paste0("../data/", df[["path"]])
+old_dir <- "./data/reduced_alph_enc_alph4_const/"
+new_dir <- "./data/reduced_alph_enc_alph4_const_unigram/"
+paths <- dir(old_dir)
 
-add_unigrams(ngram_matrices_paths, letters[1:6])
+df <- read.csv(paste0(old_dir, paths[grepl("csv", paths)]))
+
+RDSnames <- sapply(strsplit(x = df[["path"]], split = "/"), function(x) x[[3]])
+
+
+add_unigrams(old_dir, new_dir, RDSnames, letters[1:4])
+
+# reduced_alph_enc_amylogram_encoding dataset broken
+
