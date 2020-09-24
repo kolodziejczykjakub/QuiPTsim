1eed16148ec9bcc6b04e66ea4da54171009b1bd6
jakubkala <kalajakub96@gmail.com>
2020-09-24 20:05:04 +0200

    drake plan

--- inst/plan.R
+++ inst/plan.R
@@ -1,4 +1,5 @@
 library(drake)
+library(QuiPTsim)
 
 plan <- drake_plan(
   # data frame with paths of each RDS file containing n-gram counts
@@ -8,39 +9,41 @@ plan <- drake_plan(
   paths = as.character(df[df$l_seq == 10 & df$n_motifs == 3, "path"])[1],
 
   # benchmark data for various methods
-  bm_data_QuiPT  = create_benchmark_data(paths, list(method="QuiPT")),
-  bm_data_Chi  = create_benchmark_data(paths, list(method="Chi-squared")),
-  bm_data_FCBF  = create_benchmark_data(paths, list(method="FCBF")),
-  bm_data_gainratio  = create_benchmark_data(paths, list(method="gainratio")),
-  bm_data_MRMR  = create_benchmark_data(paths, list(method="MRMR")),
+  data_QuiPT  = create_benchmark_data(paths, list(method="QuiPT")),
+  data_Chi  = create_benchmark_data(paths, list(method="Chi-squared")),
+  data_FCBF  = create_benchmark_data(paths, list(method="FCBF")),
+  data_gainratio  = create_benchmark_data(paths, list(method="gainratio")),
+  data_MRMR  = create_benchmark_data(paths, list(method="MRMR")),
 
   # results aggregating
-  bm_results_QuiPT = benchmark_summary(bm_data_QuiPT,
+  results_QuiPT = benchmark_summary(data_QuiPT,
                                               list(method = "QuiPT",
-                                                   pval_thresholds = c(0.05, 0.01),
+                                                   pval_thresholds = c(0.025, 0.01),
                                                    pval_adjustments = c("", "BH"))),
-  bm_results_FCBF = benchmark_summary(bm_data_FCBF, list(method = "FCBF")),
+  results_FCBF = benchmark_summary(data_FCBF, list(method = "FCBF")),
 
-  bm_results_Chi = benchmark_summary(bm_data_Chi,
+  results_Chi = benchmark_summary(data_Chi,
                                             list(method = "Chi-squared",
-                                                 pval_thresholds = c(0.05, 0.01),
+                                                 pval_thresholds = c(0.025, 0.01),
                                                  pval_adjustments = c("", "BH"))),
+  results_gainratio = benchmark_summary(data_gainratio,
+                                           list(method = "gainratio",
+                                                fraction = 0)),
 
-  bm_results_QuiPT2 = benchmark_summary(bm_data_QuiPT,
-                                              list(method = "QuiPT",
-                                                   pval_thresholds = c(0.05, 0.01),
-                                                   pval_adjustments = c("", "BH"))),
-  results = rbind(cbind(method_name = "QuiPT", bm_results_QuiPT),
-                   cbind(method_name = "Chi-squared", bm_results_Chi))
-
+  results = rbind(cbind(method_name = "QuiPT", results_QuiPT),
+                  cbind(method_name = "Chi-squared", results_Chi),
+                  cbind(method_name = "FCBF", pval_thresholds = "",
+                        pval_adjustments = "", results_FCBF))
 )
-
 
 make(plan
      # HPC setup
      # ,parallelism = "clustermq",
-     # jobs = 4,
+    #jobs = 2,
      # console_log_file = "drake.log"
      )
 
 vis_drake_graph(plan)
+
+
+
