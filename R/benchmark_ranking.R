06d2a09672f16882099fd5e3abf70f4daf5b19e5
jakubkala <kalajakub96@gmail.com>
2020-10-06 18:32:57 +0200

    model approach - FCBF working example

--- R/benchmark_ranking.R
+++ R/benchmark_ranking.R
@@ -1,41 +1,28 @@
-library(QuiPTsim)
-library(pbapply)
 
-df <- read.csv("./reduced_alph_enc_alph4_const/alph4_const.csv")
+evaluate_selected_kmers <- function(df, validation_scheme) {
 
-paths <- df[df$l_seq == 10 & df$n_motifs == 2, "path"]
-setup <- list(method = "FCBF")
-validation_scheme <- list(type = "cv",
-                          folds = 5,
-                          learner = "classif.log_reg")
+  folds <- createFolds(y = df[["y"]],
+                       k = validation_scheme[["folds"]])
 
-m <- read_ngram_matrix(paths[1])
-filtering_results <- filter_ngrams(m,setup[["method"]])
+  results <- lapply(folds, function(fold_indices) {
 
-X <- as.matrix(m[, filtering_results[["score"]]])
-y <- attr(m, "target")
-df <- data.frame(X, y)
+    df_train <- df[-fold_indices, ]
+    df_test <- df[fold_indices, ]
 
-df
-cvReplications <- 1
-n_folds = 5
-for (i in 1:cvReplications) {
+    # linear model
+    logReg <- glm(y ~ ., family = binomial(link = "logit"), data = df_train)
 
+    y_true <- df_test[["y"]]
+    y_proba <- predict(logReg, df_test)
+    y_pred <- y_proba > 0.5
+
+
+    c(compute_metrics(y_true, y_pred),
+                 auc = auc(y_true, y_proba))
+  })
+
+  do.call(rbind, results)
 }
-folds <- createFolds(y = df$y, k = n_folds)
-
-for (foldNum in 1:n_folds) {
-
-  df_train <- df[-folds[[foldNum]], ]
-  df_test <- df[folds[[foldNum]], ]
-}
-
-# linear model
-logReg <- glm(y ~ ., family = binomial(link = "logit"), data = df_train)
-
-y_true <- df_test[["y"]]
-y_proba <- predict(logReg, df_test)
-y_pred <- y_proba > 0.5
 
 
 evaluate_filtering_results <- function(m, filtering_results, setup, validation_scheme){
@@ -78,15 +65,6 @@ evaluate_filtering_results <- function(m, filtering_results, setup, validation_s
   # results <- evaluate_selected_kmers(df, validation_scheme)
 
 }
-
-evaluate_selected_kmers <- function(df, validation_scheme) {
-
-  # caret::createFolds
-  # Stratified
-
-
-}
-
 
 ranking_summary <- function(paths, setup, validation_scheme) {
 
