980a318f87dac3c09d745d407362aaac0c79baf4
jakubkala <kalajakub96@gmail.com>
2021-02-02 11:29:05 +0100

    tryCatch deleted

--- R/benchmark_ranking.R
+++ R/benchmark_ranking.R
@@ -214,28 +214,17 @@ filter_rankings <- function(paths, output_prefix, feature_selection_method, n, f
     
     m <- read_ngram_matrix(paths[[i]], n, fraction)
 
-    tryCatch({
-      
-      filtering_results <- filter_ngrams(m, feature_selection_method = feature_selection_method)
-      results <- evaluate_filtering_results(m,
-                                            filtering_results,
-                                            list(method = feature_selection_method),
-                                            validation_scheme)
-      
-      toSave <- list(filtering_results = filtering_results, results = results)
-      
-      saveRDS(toSave,
-              output_paths[[i]])
-      message(paste0("File ", i, " saved in directory: ", output_paths[[i]]))
-    }, 
-    error = function(error_cond) {
-      warning(paste0("Error in "), paths[[i]])
-      message(error_cond)
-    },
-    warning = function(warning_cond) {
-      warning(paste0("Warning in "), paths[[i]])
-      message(warning_cond)
-    })
+    filtering_results <- filter_ngrams(m, feature_selection_method = feature_selection_method)
+    results <- evaluate_filtering_results(m,
+                                          filtering_results,
+                                          list(method = feature_selection_method),
+                                          validation_scheme)
+    
+    toSave <- list(filtering_results = filtering_results, results = results)
+    
+    saveRDS(toSave,
+            output_paths[[i]])
+    message(paste0("File ", i, " saved in directory: ", output_paths[[i]]))
   }
   
   output_paths
@@ -260,32 +249,21 @@ filter_nonrankings <- function(paths, output_prefix, feature_selection_method, n
     
     m <- read_ngram_matrix(paths[[i]], n, fraction)
     
-    tryCatch({
-      
-      filtering_results <- filter_ngrams(m, feature_selection_method = feature_selection_method)
-      validation_scheme[["n_kmers"]] <- kmers_for_nonranking_methods(filtering_results, 
-                                                                     feature_selection_method,
-                                                                     thresholds)
-      results <- evaluate_filtering_results(m,
-                                            filtering_results,
-                                            list(method = feature_selection_method),
-                                            validation_scheme)
-      
-      toSave <- list(filtering_results = filtering_results, results = results)
-      
-      saveRDS(toSave,
-              output_paths[[i]])
-      message(paste0("File ", i, " saved in directory: ", output_paths[[i]]))
-      
-    }, 
-    error = function(error_cond) {
-      warning(paste0("Error in "), paths[[i]])
-      message(error_cond)
-    },
-    warning = function(warning_cond) {
-      warning(paste0("Warning in "), paths[[i]])
-      message(warning_cond)
-    })
+    filtering_results <- filter_ngrams(m, feature_selection_method = feature_selection_method)
+    validation_scheme[["n_kmers"]] <- kmers_for_nonranking_methods(filtering_results, 
+                                                                   feature_selection_method,
+                                                                   thresholds)
+    results <- evaluate_filtering_results(m,
+                                          filtering_results,
+                                          list(method = feature_selection_method),
+                                          validation_scheme)
+    
+    toSave <- list(filtering_results = filtering_results, results = results)
+    
+    saveRDS(toSave,
+            output_paths[[i]])
+    message(paste0("File ", i, " saved in directory: ", output_paths[[i]]))
+    
   }
   output_paths
 }
